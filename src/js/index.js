9a3d1ba9fd0e2d16d64a1111d058c65fd7fc3f98
Dev-Fiao <166238040+Dev-Fiao@users.noreply.github.com>
2024-04-09 19:48:47 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,10 @@
 /* comentários */
 
-const btnMais = document.querySelector('.btn-mostrar-projetos');
 //const btnMais = document.getElementById('.btn-mostrar-projetos');
-
+const btnMais = document.querySelector('.btn-mostrar-projetos');
 const btnMenos = document.querySelector('.btn-esconder-projetos');
-
 const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
-
+const btnInicio = document.querySelector('.btn-inicio');
 
 btnMais.addEventListener('click', () => {
     MostraMaisProjetos();
@@ -16,6 +14,11 @@ btnMais.addEventListener('click', () => {
 btnMenos.addEventListener('click', () => {
     MostrarMenosProjetos();
 });
+
+btnInicio.addEventListener('click', () =>{
+    window.scrollTo(0, 0);
+})
+
 
 //transformar os códigos acima em funções
 
