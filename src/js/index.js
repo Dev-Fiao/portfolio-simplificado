08231088ad6c934b9636939e405aac0e697f944c
Dev-Fiao <166238040+Dev-Fiao@users.noreply.github.com>
2024-04-09 19:11:49 -0300

    Add files via upload

--- src/js/index.js
+++ src/js/index.js
@@ -3,28 +3,43 @@
 const btnMais = document.querySelector('.btn-mostrar-projetos');
 //const btnMais = document.getElementById('.btn-mostrar-projetos');
 
-const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)')
+const btnMenos = document.querySelector('.btn-esconder-projetos');
+
+const projetosInativos = document.querySelectorAll('.projeto:not(.ativo)');
+
 
 btnMais.addEventListener('click', () => {
-
     MostraMaisProjetos();
+});
 
-    esconderBotao();
 
+btnMenos.addEventListener('click', () => {
+    MostrarMenosProjetos();
 });
 
 //transformar os códigos acima em funções
 
 function MostraMaisProjetos() {
+
     projetosInativos.forEach(projetoInativo => {
         projetoInativo.classList.add('ativo');
     });
-}
+    var heightPage = document.body.scrollHeight;
+    window.scrollTo(0, heightPage);
 
-function esconderBotao() {
     btnMais.classList.add('remover');
+    btnMenos.classList.remove('remover');
 }
 
+function MostrarMenosProjetos() {
+    projetosInativos.forEach(projetoInativo => {
+
+        projetoInativo.classList.remove('ativo');
+    });
+
+    btnMenos.classList.add('remover');
+    btnMais.classList.remove('remover');
+}
 
 
 
